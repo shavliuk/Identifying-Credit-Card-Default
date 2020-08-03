0e47276a7eea9e6684e0ee2a3011175f0ab012fb
shavliuk <66632536+shavliuk@users.noreply.github.com>
2020-08-03 12:03:39 +0300

    Update Identifying Credit Default.py

--- Identifying Credit Default.py
+++ Identifying Credit Default.py
@@ -1,30 +1,16 @@
 import pandas as pd
-import numpy as np
 import missingno as msno
 import matplotlib.pyplot as plt
 import pandas_profiling as pp
-from pandas_profiling import ProfileReport
-from sklearn.model_selection import (train_test_split, StratifiedKFold)
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 from sklearn.svm import SVC
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import VotingClassifier
 from lightgbm import LGBMClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import StackingClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.decomposition import PCA
 
-df = pd.read_csv("C:\\Users\shavl\PycharmProjects\Identifying Credit Default/UCI_Credit_Card.csv", index_col=0, na_values='')
+
+df = pd.read_csv("UCI_Credit_Card.csv", index_col=0, na_values='')
 
 X = df.copy()
 y = X.pop('default.payment.next.month')
@@ -38,28 +24,64 @@ print(df.describe().transpose().round(2))
 msno.matrix(X)
 plt.show()
 
+report = pp.ProfileReport(df)
+report.to_file('profile_report.html')
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, stratify=y, random_state=42)
 
 xgb_clf = XGBClassifier(n_estimators=100, learning_rate=0.1, gamma=0.7, subsample = 0.8, max_depth=6, min_child_weight=3, random_state=0)
 xgb_clf.fit(X_train, y_train)
 y_pred = xgb_clf.predict(X_test)
+
+accuracy = metrics.accuracy_score(y_pred, y_test)
 recall = metrics.recall_score(y_pred, y_test)
+precision = metrics.precision_score(y_pred, y_test)
+f1 = metrics.f1_score(y_pred, y_test)
+
+print(f"XGBoost accuracy score: {accuracy:.4f}")
 print(f"XGBoost recall score: {recall:.4f}")
+print(f"XGBoost precision score: {precision:.4f}")
+print(f"XGBoost f1 score: {f1:.4f}")
 
 lgb_clf = LGBMClassifier()
 lgb_clf.fit(X_train, y_train)
 y_pred = lgb_clf.predict(X_test)
+
+accuracy = metrics.accuracy_score(y_pred, y_test)
 recall = metrics.recall_score(y_pred, y_test)
-print(f"LightBoost recall score: {recall:.4f}")
+precision = metrics.precision_score(y_pred, y_test)
+f1 = metrics.f1_score(y_pred, y_test)
+
+print(f"LightGBM accuracy score: {accuracy:.4f}")
+print(f"LightGBM recall score: {recall:.4f}")
+print(f"LightGBM precision score: {precision:.4f}")
+print(f"LightGBM f1 score: {f1:.4f}")
 
 svm_clf = SVC(kernel='rbf', random_state=0, gamma=0.5, C=1.0)
 svm_clf.fit(X_train, y_train)
 y_pred = svm_clf.predict(X_test)
+
+accuracy = metrics.accuracy_score(y_pred, y_test)
 recall = metrics.recall_score(y_pred, y_test)
+precision = metrics.precision_score(y_pred, y_test)
+f1 = metrics.f1_score(y_pred, y_test)
+
+print(f"SVM accuracy score: {accuracy:.4f}")
 print(f"SVM recall score: {recall:.4f}")
+print(f"SVM precision score: {precision:.4f}")
+print(f"SVM f1 score: {f1:.4f}")
 
 gnb_clf = GaussianNB()
 gnb_clf.fit(X_train, y_train)
 y_pred = gnb_clf.predict(X_test)
+
+accuracy = metrics.accuracy_score(y_pred, y_test)
 recall = metrics.recall_score(y_pred, y_test)
+precision = metrics.precision_score(y_pred, y_test)
+f1 = metrics.f1_score(y_pred, y_test)
+
+print(f"NaiveBayes accuracy score: {accuracy:.4f}")
 print(f"NaiveBayes recall score: {recall:.4f}")
+print(f"NaiveBayes precision score: {precision:.4f}")
+print(f"NaiveBayes f1 score: {f1:.4f}")
+
